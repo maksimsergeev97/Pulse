655683fda2b31c6abeced7f8cdfd74dab5cdfb44
maksimsergeev97 <84066632+maksimsergeev97@users.noreply.github.com>
2021-06-23 17:50:49 +0300

    add tabs on site

--- js/script.js
+++ js/script.js
@@ -15,13 +15,32 @@
 //       });
 // });     
 $(document).ready(function(){
+
   $('.owl-carousel').owlCarousel({
     loop:true,
     items: 1,
     dots: false,
     nav: true,
-    navText: ["",""],
+    navText: ["",""], 
     autoplay:true,
     autoplayTimeout:1500,
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toogleSlide(item){
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');  
+      });  
+    });
+  }
+  toogleSlide('.catalog-item__link');
+  toogleSlide('.catalog-item__back');
 });
