700eb84d5c2a54abb63345313d01741c08c650c0
maksimsergeev97 <84066632+maksimsergeev97@users.noreply.github.com>
2021-06-25 09:48:36 +0300

    add modal windows

--- js/script.js
+++ js/script.js
@@ -13,6 +13,9 @@
 //           }
 //         ]
 //       });
+
+// const { on } = require("gulp");
+
 // });     
 $(document).ready(function(){
 
@@ -43,4 +46,20 @@ $(document).ready(function(){
   }
   toogleSlide('.catalog-item__link');
   toogleSlide('.catalog-item__back');
+
+  //modal 
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+
+  $('.button_mini').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
 });
